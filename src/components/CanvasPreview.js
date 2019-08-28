bff76d583687c956d3f0fdff47ac30b566e8bdc6
hdmi <c.angorrilla@gmail.com>
2019-08-29 00:53:12 +0200

    Part3: Sandboxing js code in iframe

--- src/components/CanvasPreview.js
+++ src/components/CanvasPreview.js
@@ -1,67 +1,66 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {editError} from '../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { editError } from "../actions";
 
 class CanvasPreview extends React.Component {
-
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
     this.iframeRef = React.createRef();
+    this.imgRef = React.createRef();
+
+    window.addEventListener("message", this.receiveMessage, false);
   }
 
-  onClickCanvas = (e) => {
-    const a = document.createElement('a');
-    let image = this.canvasRef.current.toDataURL("image/png")
+  receiveMessage = event => {
+    if (!event.isTrusted) {
+      return;
+    }
+    this.imgRef.current.src = event.data;
+  };
+
+  onClickCanvas = e => {
+    const a = document.createElement("a");
+    let image = this.canvasRef.current.toDataURL("image/png");
     image = image.replace("image/png", "image/octet-stream");
-    a.setAttribute('download', 'wallpaper.png');
-    a.setAttribute('href', image);
+    a.setAttribute("download", "wallpaper.png");
+    a.setAttribute("href", image);
     a.click();
-  }
+  };
 
   //TODO: Draw canvas on state update
   componentDidMount() {
-    console.log('Canvas MOUNTED')
-    this.iframeRef.current.contentWindow.postMessage(JSON.stringify(this.props.wallpaper), '*')
+    console.log("Canvas MOUNTED");
+    this.iframeRef.current.contentWindow.postMessage(
+      JSON.stringify(this.props.wallpaper),
+      "*"
+    );
   }
 
   componentDidUpdate() {
-    console.log('Canvas UPDATED')
-    this.iframeRef.current.contentWindow.postMessage(JSON.stringify(this.props.wallpaper), '*')
+    console.log("Canvas UPDATED");
+    this.iframeRef.current.contentWindow.postMessage(
+      JSON.stringify(this.props.wallpaper),
+      "*"
+    );
   }
 
-
-
-  clientRunJS(code, canvasRef){
-    return Function('"use strict";return function(c, palette){' + code + '}', )()(canvasRef.current, this.props.wallpaper.palette);
-  }
-
-/*
-<iframe src="javascript:void(0);" ref={this.iframeRef}>
-  <canvas
-    ref={this.canvasRef}
-    height={height*ratio}
-    width={width*ratio}
-    style={{width: '90%'}}
-    onClick={this.onClickCanvas} />
-</iframe>
-
-*/
   render() {
-    const {height, width, ratio} = this.props.wallpaper;
+    const { height, width, ratio } = this.props.wallpaper;
 
     return (
       <div>
         <h4>CanvasPreview</h4>
         <div className="ui center aligned segment">
-          <div style={{width:"90%", height:"100%", display:"block"}}>
-          <iframe src="./canvasIframe.html"
-            width="90%"
-            display="inline"
-            style={{overflow:"auto"}}
-            frameBorder="0"
-            ref={this.iframeRef}/>
-        </div>
+          <div style={{ height: "100%", display: "block" }}>
+            <iframe
+              src="./canvasIframe.html"
+              sandbox="allow-scripts"
+              ref={this.iframeRef}
+              style={{ display: "none" }}
+            />
+            <img ref={this.imgRef} style={{ width: "100%" }} />
+          </div>
           <div className="">
             <span>{`${height} x ${width}`}</span>
           </div>
@@ -69,10 +68,12 @@ class CanvasPreview extends React.Component {
       </div>
     );
   }
-
 }
-const mapStateToProps = (state) => {
-  return {wallpaper: state.canvasReducer.wallpaper};
-}
+const mapStateToProps = state => {
+  return { wallpaper: state.canvasReducer.wallpaper };
+};
 
-export default connect(mapStateToProps, {editError})(CanvasPreview);
+export default connect(
+  mapStateToProps,
+  { editError }
+)(CanvasPreview);
